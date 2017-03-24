dcce32100817c99db926671a609f79a0922d9e65
Jhon Marreros Guzman <j_marreros@hotmail.com>
2017-03-24 09:13:09 -0500

    Proyecto inicial theme

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Adaptativo Theme Customizer
+ *
+ * @package Adaptativo
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function adaptativo_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'adaptativo_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function adaptativo_customize_preview_js() {
+	wp_enqueue_script( 'adaptativo_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'adaptativo_customize_preview_js' );
