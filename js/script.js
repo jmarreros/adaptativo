f0401b661f01e0eaf86fdce8863e641ebb91df8a
Jhon Marreros Guzman <j_marreros@hotmail.com>
2017-03-31 12:01:05 -0500

    Finalizado menú responsive

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+
+  // //Add icon responsive navigation
+  // $itemSel = $('.main-navigation .nav-menu li.menu-item-has-children');
+  //
+  // $itemSel.append('<span class="nav-more">+</span>').click(function(){
+  //     $(this).siblings('ul').toggle();
+  //   }
+  // );
+
+
+});
